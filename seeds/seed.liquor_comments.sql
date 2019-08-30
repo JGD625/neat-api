14f2bbe2cfbf189582563f336417b1d567c8109c
Julie Butisbauch <harukaju@gmail.com>
2019-08-30 18:39:37 -0400

    seeds finished

--- seeds/seed.liquor_comments.sql
+++ seeds/seed.liquor_comments.sql
@@ -1,6 +1,10 @@
 INSERT INTO liquor_comments (id, text, liquor_id) VALUES
-('72e5e050-d3e6-4938-814b-f8227c5df582', 'Wow', '7c698932-fab8-4054-bde9-bf6e5a9e6db5'),
-('9a20aa5e-142e-11e9-ab14-d663bd873d93', 'Cool!', '7c698932-fab8-4054-bde9-bf6e5a9e6db5'),
-('b8e8bf7a-ab2e-4937-a47f-eb18de5f46c7', 'Amazing!', '7c698932-fab8-4054-bde9-bf6e5a9e6db5'),
-('01b51c82-c98d-4c7a-a3fb-eecb89a5ad3e', 'Brilliant', '78e4bbe9-d8a7-4542-9cfc-12537328cd9d'),
-('6b58b958-b87c-4183-bcc5-0c39328330e4', 'Commentable', '78e4bbe9-d8a7-4542-9cfc-12537328cd9d');
+('72e5e050-d3e6-4938-814b-f8227c5df582', 'Elijah is a favorite in our house!', '52c3r061-j4e6-7432-457e-c8239c3tj996'),
+('9a20aa5e-142e-11e9-ab14-d663bd873d93', 'Went to the Dickel distillery for a tour, it was so cool!', '63t3c023-s4j8-1936-863a-t4293c3ty874'),
+('b8e8bf7a-ab2e-4937-a47f-eb18de5f46c7', 'Amazing! One of my favorites.', '82a3p095-k4t9-4534-834z-f1286c7tf471'),
+('72z3p064-k4r8-1533-645t-s9297c5tg698', 'Brilliant flavor and worth the price.', '72z3p084-k4r8-5533-645q-s9297c5tg688'),
+('5b48g755-b55u-4883-bcc5-2d39328330e1', 'Not great, but I do love the novelty of the Hopi corn.', '12i5c157-a2e7-5938-865w-r9217c6da582'),
+('4b88f856-b44j-4783-bcc5-8c39328330e2', 'Buffalo Trace is probably the best economy whisky out there.', '62f0k053-h9e1-1737-511a-b4257c9yf527'),
+('3b98d757-b37h-4683-bcc5-7d39328330e3', 'This rye is really trendy right now.', '82s2o051-j6y0-2634-723x-c2277c7gd553'),
+('2b28s658-b29d-4283-bcc5-4c39328330e6', 'Classic', '48n3g127-b4u8-6436-938x-n2243c6vl356'),
+('1b18a459-b18e-4383-bcc5-3f39328330e5', 'My go to for cocktails', '48n3g127-b4u8-6436-938x-n2243c6vl356');
--- seeds/seed.neat_users.sql
+++ seeds/seed.neat_users.sql
@@ -21,7 +21,7 @@ INSERT INTO neat_users (
     'I tended bar in Brooklyn for seven years, and I really learned to love American whiskies. I am doing some research with hopes of opening my own place, and I want to specialize in bourbons.'
   ),
   (
-    'fcaab65c-d073-44d4-8177-ggf9c0230a77',
+    'xcatb65c-d073-44d4-8177-ggf9c0230a77',
     'Julie',
     'Butisbauch',
     'jdbutisbauch@gmail.com',
@@ -75,25 +75,29 @@ INSERT INTO neat_articles (
   id,
   title,
   content,
-  author_id
+  author_id,
+  banner_url
 ) VALUES
   (
     'a7f400c6-8c29-46d0-b755-6239507df45e',
-    'First post with an author',
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, ut atque. Minima beatae ipsa ratione. Dolorem suscipit ullam temporibus repellendus quae atque expedita architecto corrupti doloremque ducimus eaque, eum ipsum!',
-    'fcaab65c-d073-44d4-8177-ggf9c0230a77'
+    'Japanese Whisky: the art of fine whiskey making.',
+    'When we think of whiskey, many of us think of the American varietals of bourbons and ryes, or perhaps European scotch comes to mind.  But do you think of the East? Japan has developed a really wonderful and rich history of whiskey making all of its own.  For over 100 years, Japanese distilleries have been producing some excellent expressions.  Many scotch fans would do well to try the Hokkaido produced varieties as the northern island has similar weather and landscape to Scotland. The smokey and earthy flavors will be familiar to many seasoned drinkers.  For a softer side, look to the distilleries from the main island of Honshu, where the whiskey often takes on more floral and fruity notes.',
+    '5b6620d4-1475-4f4a-b8dc-d1fa4c41023d',
+    'http://www.jdbutisbauch.com/images/art_jpns.png'
   ),
   (
     '7b635a3f-ca35-4f46-9cc3-0c30e0ff4e30',
-    'Second post with an author',
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam minus esse, dolorum vitae neque ullam adipisci consequatur doloremque autem rerum doloribus officiis exercitationem culpa, temporibus eligendi, assumenda ex. Cupiditate, sequi.',
-    '5b6620d4-1475-4f4a-b8dc-d1fa4c41023d'
+    'Building Your Own Home Bar',
+    'Building a bar in your home can be a difficult but rewarding task. There is nothing quite as impressive to your guests as a beautifully stocked bar with clever design.  One of the first things you will have to decide for yourself, is what style of bar fits in your home.  You don''t want to spend a lot of time and money creating an eye sore. For a more vintage and rustic feel, look for materials like reclaimed wood, iron, and brass. Don''t over finish anything, and leave as much raw as possible.  For a more modern look, try for nickel and glass.  Another possibly cheap and fun option is to repurpose other furniture into a bar.  Vintage pieces can be easy to come by in yard sales or flea markets, but a coat of paint and some new knobs can really make it shine.',
+    'fcdab65c-d063-45d4-8277-fff9c0230a88',
+    'http://www.jdbutisbauch.com/images/art_homebar.png'
   ),
   (
     '2acbdb56-2889-4589-8f29-39760170cba4',
-    'Third post with an author',
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut et, eum id distinctio vel sint nulla dolorum vero perspiciatis debitis repudiandae officiis nostrum illo adipisci minus placeat in error. Voluptatum.',
-    'fcdab65c-d063-45d4-8277-fff9c0230a88'
+    'Choosing Garnishes',
+    'What is a Bloody Mary without the celery? A martini without the olives? The proper garnish can make or break a cocktail, and this can be particularly important when thinking of whiskey.  For one thing, whiskey cocktails come in an quite a variety.  Even the same cocktail can vary wildly by selecting different whiskey expressions to create it. A classic Old Fashioned calls for orange and maraschino cherries, but changing it up a bit depending on the bourbon profile can make for a more perfect experience. For example, if you were to use Eagle Rare, a more chocolatey and rich bourbon, you may want to try candied cherries instead for a dessert like experience. Or, if you were to try a higher proof option like Knobb Creek, using a more sticky sweet citrus like tangerine could temper the burn. Choosing the right garnish can be difficult, but if you really consider the qualities of the whiskey expression first, I am sure you will be glad you did.',
+    '5b6620d4-1475-4f4a-b8dc-d1fa4c41023d',
+    'http://www.jdbutisbauch.com/images/art_garnish.png'
   );
 
 
@@ -111,25 +115,25 @@ INSERT INTO article_comments (
   ),
   (
     '032e0655-3e0b-4469-bc43-b4e860baa0e1',
-    'Another comment from a user',
+    'Wow! I should try this!',
     'a7f400c6-8c29-46d0-b755-6239507df45e',
     'f19d7725-c447-42d0-8b81-f41a16a97c34'
   ),
   (
     '269bb863-ad3b-4f9a-a6f0-22a7b426ffff',
-    'I never really looked at it that way.',
+    'I never really looked at it that way. I think I am going to garage sales soon!',
     '7b635a3f-ca35-4f46-9cc3-0c30e0ff4e30',
     'fcdab65c-d063-45d4-8277-fff9c0230a88'
   ),
   (
     '6f49cc70-e24d-422b-8385-e8d46c071d97',
-    'More comments by a user',
+    'I find Japanese whisky too expensive, but I did like it when I tried it.',
     'a7f400c6-8c29-46d0-b755-6239507df45e',
     '821a24ae-40ad-4f04-ad46-efbb5322a1bb'
   ),
   (
     '826801e3-e062-4e3f-b602-976aaf49ad74',
-    'Commentable comment from user',
+    'I build mine out of an old, broken piano!',
     '7b635a3f-ca35-4f46-9cc3-0c30e0ff4e30',
     'f19d7725-c447-42d0-8b81-f41a16a97c34'
   );
@@ -137,37 +141,37 @@ INSERT INTO article_comments (
   INSERT INTO liquor_comments (
   id,
   text,
-  article_id,
+  liquor_id,
   user_id
 ) VALUES
   (
     'fabb14e1-2ac2-4c9c-8330-453fdb57ca02',
-    'Comment with a user',
-    'a7f400c6-8c29-46d0-b755-6239507df45e',
-    'fcdab65c-d063-45d4-8277-fff9c0230a88'
+    'Everything they make is so darn good',
+    '64o3h725-j4w8-8235-824e-m6913c4to634',
+    '2ad8f2bc-23db-4ace-aa9d-4da4844da0f8'
   ),
   (
     '032e0655-3e0b-4469-bc43-b4e860baa0e1',
-    'Another comment from a user',
-    'a7f400c6-8c29-46d0-b755-6239507df45e',
+    'The only Nikka in my budget, but still very good.',
+    '84q3d125-u4s6-6135-443u-b7413c1ae576',
     'f19d7725-c447-42d0-8b81-f41a16a97c34'
   ),
   (
     '269bb863-ad3b-4f9a-a6f0-22a7b426ffff',
-    'Users comment',
-    '7b635a3f-ca35-4f46-9cc3-0c30e0ff4e30',
-    'fcdab65c-d063-45d4-8277-fff9c0230a88'
+    'I would give anything to try this!',
+    '91x3d122-a4s3-6939-235z-i3413c1np192',
+    '5b6620d4-1475-4f4a-b8dc-d1fa4c41023d'
   ),
   (
     '6f49cc70-e24d-422b-8385-e8d46c071d97',
-    'More comments by a user',
-    'a7f400c6-8c29-46d0-b755-6239507df45e',
-    '821a24ae-40ad-4f04-ad46-efbb5322a1bb'
+    'Gotta love my Irish whisky!',
+    '29x3d124-c4s9-1637-446w-k4513c1yn042',
+    '2ad8f2bc-23db-4ace-aa9d-4da4844da0f8'
   ),
   (
     '826801e3-e062-4e3f-b602-976aaf49ad74',
-    'Commentable comment from user',
-    '7b635a3f-ca35-4f46-9cc3-0c30e0ff4e30',
+    'SUUUUUCH a goooood SCOTCH',
+    '86g3d127-n4s9-1633-116g-t7413c1ei638',
     'f19d7725-c447-42d0-8b81-f41a16a97c34'
   );
 
